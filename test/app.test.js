9499bfb9b7108098ed49168c049c233071cd43d4
Ganesh <ganeshsg430@gmail.com>
2025-01-01 21:39:18 +0530

    app.test.js created

---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+const express = require('express');
+const app = require('../app');
+
+describe('GET /', () => {
+  it('should return 200 and expected message', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toBe('Hello, CI/CD Pipeline with Jenkins!');
+  });
+});
